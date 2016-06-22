042dfbf1706e679b56dc64ff57f099b8075b09d9
Luiz Roberto Lethang Rodolpho <luiz.rodolpho@itsmobile.net.br>
2016-06-21 22:14:20 -0300

    teste

---
+++ main.js
@@ -0,0 +1,64 @@
+var request = require('request'); 		// requisições
+var express = require('express');		// routes
+var jsdom 	= require('jsdom');			// carrega response com JQuery 
+var iconv  	= require('iconv-lite');	// encoding
+var app 	= module.exports = express();
+var URL 	= "http://sc.olx.com.br/florianopolis-e-regiao/leste/imoveis/aluguel";
+
+try
+{
+	app.get('/', function(req, res) {
+		console.log('[GET] /');
+	});
+	
+	app.get('/olx', function(req, res){
+		getOLX(res);
+	});
+	
+	app.listen(3001, function () {
+		console.log('Example app listening on port 3001!');
+	});
+}
+catch (e)
+{
+	console.log(e);
+}
+
+function getOLX(res) {
+	// propriedades para request
+	var reqOptions = {
+			uri: URL,
+			encoding: null
+		};
+		
+	request(reqOptions, 
+			function(err, response, body)
+			{
+				// error handling
+				if(err && response.statusCode !== 200)
+				{	
+					console.log('Request error.');
+				}
+				
+				// decoding
+				body = new Buffer(body, 'binary');
+				body = iconv.decode(new Buffer(body), "ISO-8859-1");
+				
+				// parse
+				jsdom.env({
+					html: body,
+					scripts: ['http://code.jquery.com/jquery-3.0.0.min.js'],
+					done: function (err, window) {
+						var $ = window.jQuery;
+						var listLink = $(".OLXad-list-link" );
+						var content = '<table>';
+						$.each(listLink, function(index, value) {
+						  content += '<tr><td style=\'border-bottom: 1px solid lightgray;\'>' + value.title + '</td><td style=\'width: 100px; border-bottom: 1px solid lightgray;\'>' + $(value).find('.col-3').children().text() + '</td></tr>';
+						});
+						content += '</table>';
+						res.send(content);
+					}
+				});
+			}
+	);
+}
